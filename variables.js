773104258aa151621c37645135f2d605f0e70ed9
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-06-07 22:53:29 -0400

    Add java script nodes

--- variables.js
+++ variables.js
@@ -7,3 +7,9 @@ var carModel = 2019
 var married = true
 var baby = true
 var tired = true
+
+var email = "andrew_7117@gmail.com";
+var movie = "Harry potter";
+console.log(email);
+console.log(carModel)
+console.log(movie)
